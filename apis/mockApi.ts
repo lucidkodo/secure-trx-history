ec932422f16b4cd5a25c4c133dd9c93d2720dcc3
lckd <lucidkodo@gmail.com>
2024-04-08 13:55:39 +0800

    Add mock api to simulate promise resolving

---
+++ apis/mockApi.ts
@@ -0,0 +1,25 @@
+import { TransactionDetails } from '../models/Transaction';
+import { generateTransaction } from './helper';
+
+export function authenticate(data: {
+  email: string;
+  password: string;
+}): Promise<boolean> {
+  return new Promise((res, rej) => {
+    if (data.password === 'password') {
+      return res(true);
+    }
+
+    return rej('Incorrect password.');
+  });
+}
+
+export function getTransactions(): Promise<TransactionDetails[]> {
+  return new Promise((res, rej) => {
+    const data: TransactionDetails[] = [];
+    for (let i = 0; i < 20; i++) {
+      data.push(generateTransaction());
+    }
+    return res(data);
+  });
+}
